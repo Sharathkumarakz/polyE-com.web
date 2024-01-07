1afb786b5c1ca27dc418ad1e253f2ca00e77440d
Sharath Kumar A <sharathkannanakz@gmail.com>
2024-01-07 10:18:14 +0530

    product card added

---
+++ src/app/user/tabswitch/tabswitch.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabswitch',
+  templateUrl: './tabswitch.component.html',
+  styleUrl: './tabswitch.component.css'
+})
+export class TabswitchComponent {
+
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,17 +5,21 @@ import { UserRoutingModule } from './user-routing.module';
 import { HeaderComponent } from './layouts/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AddsComponent } from './adds/adds.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { TabswitchComponent } from './tabswitch/tabswitch.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
-    AddsComponent
+    AddsComponent,
+    ProductCardComponent,
+    TabswitchComponent,
   ],
   imports: [
     CommonModule,
     UserRoutingModule
-  ]
+  ]     
 })
 export class UserModule { }
