d33128104655665af3bd603b830470d7d4489b3f
Sharath Kumar A <sharathkannanakz@gmail.com>
2024-01-19 23:39:03 +0530

    old start

--- environments/environment.development.ts
+++ environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     //backend api
-    //  apiUrl: `http://localhost:5000`,
-    apiUrl:`https://impossible-fly-purse.cyclic.app`
+    apiUrl: `http://localhost:5000`,
+    // apiUrl:`https://impossible-fly-purse.cyclic.app`
 };
